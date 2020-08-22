b8c0bbf53ff0750a49e21217dcf4368b6827599f
Matt Goshorn <matt.goshorn@techelevator.com>
2020-08-22 18:53:26 -0400

    Update employee_profile model

--- backend/src/main/java/com/techelevator/employeeProfile/EmployeeProfile.java
+++ backend/src/main/java/com/techelevator/employeeProfile/EmployeeProfile.java
@@ -7,9 +7,6 @@ public class EmployeeProfile {
 
 	private int profileId;
 	private int userId;
-//	private String firstName;
-//	private String lastName;
-//	private String profilePic;
 	private String role;
 	private Date startDate;
 	private Date endDate;
@@ -21,9 +18,6 @@ public class EmployeeProfile {
 
 	public EmployeeProfile(String role, Date startDate, String campusShortCode) {
 		super();
-//		this.firstName=firstName;
-//		this.lastName=lastName;
-//		this.profilePic=profilePic;
 		this.role = role;
 		this.startDate = startDate;
 		this.campusShortCode = campusShortCode;
@@ -37,30 +31,6 @@ public class EmployeeProfile {
 	public void setProfileId(int profileId) {
 		this.profileId = profileId;
 	}
-
-//	public String getFirstName() {
-//		return firstName;
-//	}
-//
-//	public void setFirstName(String firstName) {
-//		this.firstName = firstName;
-//	}
-//
-//	public String getLastName() {
-//		return lastName;
-//	}
-//
-//	public void setLastName(String lastName) {
-//		this.lastName = lastName;
-//	}
-//
-//	public String getProfilePic() {
-//		return profilePic;
-//	}
-//
-//	public void setProfilePic(String profilePic) {
-//		this.profilePic = profilePic;
-//	}
 
 	public int getUserId() {
 		return userId;
